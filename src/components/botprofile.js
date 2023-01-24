66a37e42b0b2d49241a0658839600c624e3997f6
kipchumba-ket <kipchumbaelly97@gmail.com>
2023-01-24 08:47:19 +0300

    fixed

--- src/components/botprofile.js
+++ src/components/botprofile.js
@@ -12,7 +12,8 @@ function BotProfile({id,image,name,phrase,health,armor,damage,addBot,handleId}) 
           <img src = {image}></img>
           <h5 id ="botName">{name}</h5>
           <h7 id = "botPhrase" >{phrase}</h7>
-         <span id="abilities" ><i id="health" className="material-icons">healing</i><h5 id="ability">{health}</h5><i id="shield" className="material-icons">shield</i><h5 id="ability">{armor}</h5><i id="bolt" className="material-icons">bolt</i><h5 id="ability">{damage}</h5><button onClick = {(e)=>{
+         <span id="abilities" ><i id="health" className="material-icons">healing</i><h5 id="ability">{health}</h5><i id="shield" className="material-icons">shield</i><h5 id="ability">{armor}</h5><i id="bolt" className="material-icons">bolt</i><h5 id="ability">{damage}</h5>
+         <button onClick = {(e)=>{
         e.stopPropagation()
         console.log("onClick");
         }}id="delete">X</button></span>
